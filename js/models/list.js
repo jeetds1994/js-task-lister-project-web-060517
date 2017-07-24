67f4991b04784109a4e34b48129017994c52756e
Diljeet Singh <jeetds1994@gmail.com>
2017-07-24 14:04:30 -0400

    done

--- js/models/list.js
+++ js/models/list.js
@@ -1,2 +1,30 @@
 // List Model
 
+listIDAssigner = 0
+
+class List{
+  constructor(title){
+    this.title = title
+    this.tasks = []
+    this.id = ++listIDAssigner
+    List.all.push(this)
+  }
+
+  static find(id){
+    return List.all.filter(function(list){
+      return list.id === parseInt(id)
+    })
+  }
+
+  static findByTitle(title){
+    return List.all.filter(function(list){
+      return list.title === title
+    })
+  }
+  static delete(id){
+    List.all = List.all.filter(function(list){
+      return list.id !== id
+    })
+  }
+}
+List.all = []
